ece07acee2feb043bb2f7bcface5c8bb074bbb39
Aletia Trepte <aletia.trepte@gmail.com>
2026-06-03 13:56:32 -0700

    configured atlas network to receive write from local machine

---
+++ mongodb.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timezone
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = MongoClient(os.getenv("MONGODB_URI"))
+
+db = client["airthings_dashboard"]
+collection = db["raw_readings"]
+
+test_doc = {
+    "source": "airthings",
+    "device_id": "test_device",
+    "recorded_at": datetime.now(timezone.utc),
+    "metrics": {
+        "temp_f": 72.5,
+        "humidity": 50
+    }
+}
+
+result = collection.insert_one(test_doc)
+
+print("Inserted ID:", result.inserted_id)
+print(client.list_database_names())
